686617974414e90c9f420ac6f9643736cd2d5c8d
Jon Howard <jhoward15@gmail.com>
2023-04-06 16:26:15 -0500

    Initial

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # javascript_chrome_ext_multi_clipboards
-Chrome extension that gives you multiple clipboard accessible by hotkeys
+
+Chrome extension that gives you multiple clipboard accessible by hotkeys.
+
+# Usage
+
+# Logo
+
+"Copy icon" https://www.iconfinder.com/icons/9069845/copy_icon by ByteDance IconPark https://www.iconfinder.com/icon-park is licensed under CC BY 4.0 https://creativecommons.org/licenses/by/4.0/
+
+## Permissions required:
+
+### XYZ
---
+++ background.js
@@ -0,0 +1,39 @@
+// Listen for hotkey presses
+chrome.commands.onCommand.addListener(function (command) {
+  console.log("onInstalled " + command);
+  if (command == "copy") {
+    copyToClipboard(1);
+    clipboardNum = 1;
+    chrome.storage.local.get(["clipboard" + clipboardNum], function (result) {
+      if (result["clipboard" + clipboardNum]) {
+        console.log('Stored text in clipboard 1: " + result["clipboard" + clipboardNum]');
+      } else {
+        console.log("No text in clipboard 1");
+      }
+    });
+  } else if (command == "paste") {
+    pasteFromClipboard(1);
+  }
+});
+
+// Copy the selected text to the specified clipboard location
+function copyToClipboard(clipboardNum) {
+  console.log("copyToClipboard");
+  chrome.tabs.executeScript({
+    code:
+      "var selectedText = window.getSelection().toString(); chrome.storage.local.set({ 'clipboard" +
+      clipboardNum +
+      "': selectedText });",
+  });
+}
+
+// Paste the text from the specified clipboard location
+function pasteFromClipboard(clipboardNum) {
+  chrome.storage.local.get(["clipboard" + clipboardNum], function (result) {
+    if (result["clipboard" + clipboardNum]) {
+      chrome.tabs.executeScript({
+        code: "document.activeElement.value += '" + result["clipboard" + clipboardNum] + "';",
+      });
+    }
+  });
+}
