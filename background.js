bc01043f9bc4be704e0e0383058c46d5085cd228
Jon Howard <jhoward15@gmail.com>
2023-04-10 21:16:57 -0500

    Working 2 clipboards

--- background.js
+++ background.js
@@ -1,42 +1,54 @@
 // Listen for hotkey presses
 chrome.commands.onCommand.addListener(function (command) {
   console.log("[background.js] onInstalled. Command received: " + command);
-  if (command == "copy") {
-    copyToClipboard(1);
-    clipboardNum = 1;
-    chrome.storage.local.get(["clipboard" + clipboardNum], function (result) {
-      if (result["clipboard" + clipboardNum]) {
-        console.log("[background.js] Stored text in clipboard 1: " + result["clipboard" + clipboardNum]);
-      } else {
-        console.log("[background.js] No text in clipboard 1");
-      }
-    });
-  } else if (command == "paste") {
-    pasteFromClipboard(1);
+  // if command contains "copy" then get the number from the end of the string
+  // and use that number to store the text in the clipboard
+  if (command.includes("copy")) {
+    clipboardNum = command.substring(command.length - 1);
+    copyToClipboard(clipboardNum);
+  } else if (command.includes("paste")) {
+    clipboardNum = command.substring(command.length - 1);
+    pasteFromClipboard(clipboardNum);
   }
 });
 
 // Copy the selected text to the specified clipboard location
 function copyToClipboard(clipboardNum) {
-  console.log("[background.js] copyToClipboard before query");
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    console.log("[background.js] copyToClipboard URL: " + tabs[0].url);
-    console.log("[background.js] copyToClipboard ID: " + tabs[0].id);
-
     chrome.scripting.executeScript({
-      target: { tabId: tabs[0].id, allFrames: true },
-      files: ["content.js"],
+      args: [clipboardNum],
+      target: { tabId: tabs[0].id },
+      func: (clipboardNum) => {
+        selectedText = window.getSelection().toString();
+        chrome.storage.local.set({ ["clipboard" + clipboardNum]: [selectedText] });
+      },
     });
   });
-  console.log("[background.js] copyToClipboard after query");
 }
 
 // Paste the text from the specified clipboard location
 function pasteFromClipboard(clipboardNum) {
+  console.log("Paste from clipboard: " + clipboardNum);
   chrome.storage.local.get(["clipboard" + clipboardNum], function (result) {
     if (result["clipboard" + clipboardNum]) {
-      chrome.tabs.executeScript({
-        code: "document.activeElement.value += '" + result["clipboard" + clipboardNum] + "';",
+      console.log("Paste text from clipboard: " + result["clipboard" + clipboardNum]);
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        chrome.scripting.executeScript({
+          args: [result["clipboard" + clipboardNum]],
+          target: { tabId: tabs[0].id },
+          func: (pasteValue) => {
+            console.log("Paste text to page: " + pasteValue);
+            var input = document.activeElement;
+            var textToInsert = "hello world";
+            var cursorPosition = input.selectionStart;
+            var currentValue = input.value;
+            var newValue = currentValue.slice(0, cursorPosition) + textToInsert + currentValue.slice(cursorPosition);
+            input.value = newValue;
+            var newCursorPosition = cursorPosition + textToInsert.length;
+            input.setSelectionRange(newCursorPosition, newCursorPosition);
+            input.focus();
+          },
+        });
       });
     }
   });
--- content.js
+++
@@ -1,6 +0,0 @@
-clipboardNum = 1;
-selectedText = window.getSelection().toString();
-console.log("[content.js] selectedText:" + selectedText);
-clipboardName = "clipboard" + clipboardNum;
-chrome.storage.local.set({ clipboardName: selectedText });
-//console.log("[content.js] selectedText:" + selectedText);
