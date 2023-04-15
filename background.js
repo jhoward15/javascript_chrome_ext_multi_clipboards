aebe59403b78024b9e907ee045f8c193fcfd51a4
Jon Howard <jhoward15@gmail.com>
2023-04-15 08:01:59 -0500

    Working popup and new icon

--- background.js
+++ background.js
@@ -16,68 +16,13 @@ chrome.commands.onCommand.addListener(function (command) {
   }
 });
 
-// Display all clipboards with their values in a popup
-function displayClipboards() {
-  console.log("[10 Clips] Displaying clipboards");
-
-  console.log("[10 Clips] Clearing all 10 clipboards");
-  chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    // The allFrames:true option is required to access the content of iframes
-    chrome.scripting.executeScript({
-      args: [],
-      target: { tabId: tabs[0].id, allFrames: true },
-      func: () => {
-        function displayClipboardsPopup() {
-          let params = `scrollbars=no,resizable=no,status=no,location=no,toolbar=no,menubar=no,width=0,height=0,left=-1000,top=-1000`;
-
-          open("/", "test", params);
-
-          var popup = window.open("", "ClipboardsPopup", "width=400,height=500,menubar=no,toolbar=no");
-          popup.onload = function () {
-            popup.document.write(`
-              <html>
-                <head>
-                  <title>Clipboards</title>
-                  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-                </head>
-                <body>
-                  <div class="container">
-                    <h1 class="my-3">Clipboards</h1>
-                    ${clipboards}
-                  </div>
-                </body>
-              </html>
-            `);
-          };
-        }
-
-        var clipboards = "";
-        for (let i = 1; i <= 10; i++) {
-          chrome.storage.local.get(["10_Clips_clipboard_" + i], function (result) {
-            if (result["10_Clips_clipboard_" + i]) {
-              console.log(`[10 Clips] Clipboard ${i}: ${result["10_Clips_clipboard_" + i]}`);
-              clipboards += `Clipboard ${String(i).padStart(2, "0")}: ${result["10_Clips_clipboard_" + i]}<br>`;
-            } else {
-              clipboards += `Clipboard ${String(i).padStart(2, "0")}: <br>`;
-            }
-          });
-          if (i == 10) {
-            displayClipboardsPopup();
-          }
-        }
-      },
-    });
-  });
-}
-
 // Clear all clipboards. Primarily done this way so we can prompt the user that it has been done
 function clearAllClipboards() {
   console.log("[10 Clips] Clearing all 10 clipboards");
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
-    // The allFrames:true option is required to access the content of iframes
     chrome.scripting.executeScript({
       args: [],
-      target: { tabId: tabs[0].id, allFrames: true },
+      target: { tabId: tabs[0].id },
       func: () => {
         // Clear clipboards 1-10
         for (let i = 1; i <= 10; i++) {
@@ -98,8 +43,10 @@ function copyToClipboard(clipboardNum) {
       target: { tabId: tabs[0].id, allFrames: true },
       func: (clipboardNum) => {
         selectedText = window.getSelection ? window.getSelection().toString() : document.selection.createRange().text;
-        console.log(`[10 Clips] Copied text to clipboard ${clipboardNum}: '${selectedText}'`);
-        chrome.storage.local.set({ ["10_Clips_clipboard_" + clipboardNum]: [selectedText] });
+        if (selectedText) {
+          console.log(`[10 Clips] Copied text to clipboard ${clipboardNum}: '${selectedText}'`);
+          chrome.storage.local.set({ ["10_Clips_clipboard_" + clipboardNum]: [selectedText] });
+        }
       },
     });
   });
@@ -107,7 +54,6 @@ function copyToClipboard(clipboardNum) {
 
 // Paste the text from the specified clipboard location
 function pasteFromClipboard(clipboardNum) {
-  console.log("[10 Clips] Paste from clipboard: " + clipboardNum);
   chrome.storage.local.get(["10_Clips_clipboard_" + clipboardNum], function (result) {
     if (result["10_Clips_clipboard_" + clipboardNum]) {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
@@ -122,6 +68,7 @@ function pasteFromClipboard(clipboardNum) {
               console.log(`[10 Clips] Error getting cursor position for element: ${input}`);
               return;
             }
+            console.log("[10 Clips] Paste from clipboard: " + clipboardNum);
 
             const currentValue = input.value;
             const newValue =
--- popup.js
+++ popup.js
@@ -1,20 +1,58 @@
-var clipboard = "";
-const clipboardsList = document.getElementById("clipboards-list");
-for (let i = 1; i <= 10; i++) {
-  chrome.storage.local.get(["10_Clips_clipboard_" + i], function (result) {
-    if (result["10_Clips_clipboard_" + i]) {
-      var clipboardValue =
-        result["10_Clips_clipboard_" + i].toString().length > 30
-          ? result["10_Clips_clipboard_" + i].toString().substring(0, 30) + "..."
-          : result["10_Clips_clipboard_" + i].toString();
-      clipboard = `${String(i).padStart(2, "0")}: ${clipboardValue}`;
-    } else {
-      clipboard = `${String(i).padStart(2, "0")}:`;
-    }
-    console.log(`[10 Clips] Clipboard ${i}: ${result["10_Clips_clipboard_" + i]}`);
-    const clipboardItem = document.createElement("li");
-    clipboardItem.classList.add("list-group-item");
-    clipboardItem.innerText = clipboard;
-    clipboardsList.appendChild(clipboardItem);
-  });
+function displayClipboardContents() {
+  var clipboard = "";
+  const clipboardsList = document.getElementById("clipboards-list");
+  for (let i = 1; i <= 10; i++) {
+    chrome.storage.local.get(["10_Clips_clipboard_" + i], function (result) {
+      if (result["10_Clips_clipboard_" + i]) {
+        var clipboardValue =
+          result["10_Clips_clipboard_" + i].toString().length > 30
+            ? result["10_Clips_clipboard_" + i].toString().substring(0, 47) + "..."
+            : result["10_Clips_clipboard_" + i].toString();
+        clipboard = `<span style="color:lightgrey;">${String(i).padStart(
+          2,
+          "0"
+        )}</span>&nbsp;&nbsp;&nbsp;${clipboardValue}`;
+      } else {
+        clipboard = `<span style="color:lightgrey;">${String(i).padStart(2, "0")}</span>`;
+      }
+      console.log(`[10 Clips] Clipboard ${i}: ${result["10_Clips_clipboard_" + i]}`);
+      const clipboardItem = document.createElement("li");
+      clipboardItem.classList.add("list-group-item");
+      //clipboardItem.classList.add("h7");
+      //clipboardItem.className = ".small";
+      clipboardItem.innerHTML = clipboard;
+      clipboardsList.appendChild(clipboardItem);
+    });
+  }
 }
+
+// Clear all clipboards
+function clearAllClipboards() {
+  console.log("[10 Clips] Clearing all 10 clipboards");
+  // Clear clipboards 1-10
+  for (let i = 1; i <= 10; i++) {
+    chrome.storage.local.set({ ["10_Clips_clipboard_" + i]: "" });
+  }
+}
+
+// Initial population of the clipboard contents
+displayClipboardContents();
+
+// Get a reference to the "Close" button
+const closeButton = document.getElementById("close-button");
+
+// Add an event listener to the button
+closeButton.addEventListener("click", function () {
+  // Close the window
+  window.close();
+});
+
+// Get a reference to the "Clear All" button
+const clearAllButton = document.getElementById("clear-all-button");
+
+// Add an event listener to the button
+clearAllButton.addEventListener("click", function () {
+  // Clear all clipboards
+  clearAllClipboards();
+  window.location.reload();
+});
