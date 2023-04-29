fb91d4bc7d0d28d46a7616a5dc02037d9b803695
Jon Howard <jhoward15@gmail.com>
2023-04-29 14:52:18 -0500

    Added better copy/paste logic and badge status

--- background.js
+++ background.js
@@ -39,13 +39,17 @@ function copyToClipboard(clipboardNum) {
   chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
     // The allFrames:true option is required to access the content of iframes
     chrome.scripting.executeScript({
-      args: [clipboardNum],
-      target: { tabId: tabs[0].id, allFrames: true },
-      func: (clipboardNum) => {
+      args: [clipboardNum, tabs[0].id],
+      target: { tabId: tabs[0].id },
+      func: (clipboardNum, tabId) => {
         selectedText = window.getSelection ? window.getSelection().toString() : document.selection.createRange().text;
         if (selectedText) {
           console.log(`[10 Clips] Copied text to clipboard ${clipboardNum}: '${selectedText}'`);
           chrome.storage.local.set({ ["10_Clips_clipboard_" + clipboardNum]: [selectedText] });
+          chrome.runtime.sendMessage({ action: "updateBadge", status: "copySuccess", tab: tabId });
+        } else {
+          console.log(`[10 Clips] No selected text to copy to clipboard ${clipboardNum}`);
+          chrome.runtime.sendMessage({ action: "updateBadge", status: "copyFail", tab: tabId });
         }
       },
     });
@@ -54,21 +58,22 @@ function copyToClipboard(clipboardNum) {
 
 // Paste the text from the specified clipboard location
 function pasteFromClipboard(clipboardNum) {
+  console.log("[10 Clips] Pasting from clipboard: " + clipboardNum);
   chrome.storage.local.get(["10_Clips_clipboard_" + clipboardNum], function (result) {
     if (result["10_Clips_clipboard_" + clipboardNum]) {
       chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
         chrome.scripting.executeScript({
-          args: [result["10_Clips_clipboard_" + clipboardNum].toString()],
-          target: { tabId: tabs[0].id, allFrames: true },
-          func: (pasteValue) => {
+          args: [result["10_Clips_clipboard_" + clipboardNum].toString(), tabs[0].id],
+          target: { tabId: tabs[0].id },
+          func: (pasteValue, tabId) => {
             console.log(`[10 Clips] Paste text to page: '${pasteValue}' [length: ${pasteValue.length}]`);
             const input = document.activeElement;
             const cursorPosition = input.selectionStart;
             if (cursorPosition === undefined) {
               console.log(`[10 Clips] Error getting cursor position for element: ${input}`);
+              chrome.runtime.sendMessage({ action: "updateBadge", status: "pasteFail", tab: tabId });
               return;
             }
-            console.log("[10 Clips] Paste from clipboard: " + clipboardNum);
 
             const currentValue = input.value;
             const newValue =
@@ -77,9 +82,35 @@ function pasteFromClipboard(clipboardNum) {
             const newCursorPosition = cursorPosition + pasteValue.length;
             input.setSelectionRange(newCursorPosition, newCursorPosition);
             input.focus();
+            chrome.runtime.sendMessage({ action: "updateBadge", status: "pasteSuccess", tab: tabId });
           },
         });
       });
     }
   });
 }
+
+// Update the badge text when a message is received
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  console.log("[10 Clips] Message received: " + message.status);
+  if (message.action === "updateBadge") {
+    if (message.status === "copySuccess") {
+      chrome.action.setBadgeBackgroundColor({ color: "#3471FF", tabId: message.tab });
+      chrome.action.setBadgeText({ text: "Copy", tabId: message.tab });
+    } else if (message.status === "copyFail") {
+      chrome.action.setBadgeBackgroundColor({ color: "#FE0000", tabId: message.tab });
+      chrome.action.setBadgeText({ text: "Fail", tabId: message.tab });
+    } else if (message.status === "pasteSuccess") {
+      chrome.action.setBadgeBackgroundColor({ color: "#3471FF", tabId: message.tab });
+      chrome.action.setBadgeText({ text: "Paste", tabId: message.tab });
+    } else if (message.status === "pasteFail") {
+      chrome.action.setBadgeBackgroundColor({ color: "#FE0000", tabId: message.tab });
+      chrome.action.setBadgeText({ text: "Fail", tabId: message.tab });
+    }
+
+    // Clear the badge text after 5 seconds
+    setTimeout(() => {
+      chrome.action.setBadgeText({ text: "", tabId: message.tab });
+    }, 3000);
+  }
+});
